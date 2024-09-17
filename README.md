91988b1b6787eddd4c981de4cd2dd5ae4fb2fcc7
sidd0894 <siddharthmaurya7122005@gmail.com>
2024-09-17 13:38:12 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Password Encryption/Decryption Tool
+This is a terminal based password encryption/decryption tool.
+
+## About this Project
+This project offers an easy tool for encrypting and decrypting passwords using the `Fernet` symmetric encryption method from the `cryptography` library. It helps users safely store encrypted passwords and handle encryption keys.
+
+## How to run this project?
+To run this follow the steps given below-
+1. **Install python**: <br>
+Get python from [python.org](https://www.python.org/downloads/) if you don't have it yet. This project is compatible with Python 3.x.
+
+2. **Download or Clone the Repository**: <br>
+Download the zip of this repository  from the green 'Code' button on top or you can clone this repository using the following command:
+   ```
+   git clone <repository-url>
+   ```
+
+3. **Navigate to the directory** where the repository is saved in your system, and open terminal in that directory.
+
+4. Now run the command in the terminal:
+
+    ```
+    python pass-encrypt-decrypt.py
+    ```
---
+++ pass-encrypt-decrypt.py
@@ -0,0 +1,93 @@
+from cryptography.fernet import Fernet
+import sys
+
+
+def encrypt(password: str, filename: str):
+    key = Fernet.generate_key()
+    print(f'\n-Secret Key: {key.decode()}')
+
+    # Create a new instance of Fernet class
+    cipher_suite = Fernet(key)
+
+    encrypted_pass = cipher_suite.encrypt(password.encode())
+    print(f'-Encrypted Password: {encrypted_pass.decode()}')
+
+    # Save encrypted password and its key
+    try:
+        with open(filename, 'a') as file:
+            file.write(f'Encrypted Password: {encrypted_pass.decode()}\n')
+            file.write(f'Key: {key.decode()}\n\n')
+    except Exception as e:
+        print(f'[ERROR] Error occured while saving encrypted password: {e}')
+
+
+
+def decrypt(encrypted_pass: str, key: str):
+    try:
+        cipher_suite = Fernet(key.encode())
+
+        decrypted_password = cipher_suite.decrypt(encrypted_pass.encode())
+        print(f'\nOriginal password: {decrypted_password.decode()}')
+    except Exception as e:
+        print(f'[ERROR] Decryption failed: {e}')
+
+
+
+def main():
+    try:
+        transform_type = input('1. Encrypt\n'+
+                               '2. Decrypt\n'+
+                               'Choose option (1/2): ').strip().lower()
+
+
+        if transform_type in ['1', 'e', 'encrypt']:
+            while True:
+                password = input('Enter password to encrypt: ').strip()
+                if password == '':
+                    print('[ERROR] Password can\'t be empty')
+                else:
+                    break
+                print('\nKeyboard Interuption')
+                print('Exiting...')
+                sys.exit()
+
+            # Validate file name
+            while True:
+                filename = input('Enter filename to save encrypted text (e.g., passwords.txt): ').strip()
+                if filename == '':
+                    filename = 'passwords.txt'
+                    break
+                elif not (filename.endswith('.txt') and len(filename) > 4 and filename[-4] == '.'):
+                    print('[ERROR] Filename should be in format \'filename.txt\'')
+                else:
+                    break
+            
+
+            encrypt(password, filename)
+                
+
+        elif transform_type in ['2', 'd', 'decrypt']:
+            encrypted_pass = input('\nEnter encrypted password: ').strip()
+
+            if encrypted_pass == '':
+                print('[ERROR] Decryption text cannot be empty')
+                sys.exit()
+
+            key = input('Enter secret key: ').strip()
+            if key == '':
+                print('[ERROR] Secret key connot be empty')
+                sys.exit()
+
+            decrypt(encrypted_pass, key)
+
+
+        else:
+            print('[ERROR] Invalid option')
+            
+    except KeyboardInterrupt:
+        print('\nKeyboard Interuption')
+        print('Exiting...')
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
